ae3c4df93c29fe4cbc57ee0763e448b61d79dafa
erijonhson <erijonhson.os@gmail.com>
2015-02-17 15:27:32 -0300

    Organização da classe Character. função para detectar sucesso no ataque do personagem inserida.

--- src/Character.py
+++ src/Character.py
@@ -4,8 +4,9 @@ import pygame
 
 class Character:
 	
-	def __init__(self):
+	def __init__(self, id):
 		# essential
+		self.id = id
 		self.name = ''
 		self.life = 100
 		self.stranger = 5
@@ -36,10 +37,33 @@ class Character:
 			pygame.K_e: False
 		}
 		self.side = 'right'
+		self.attackKey = 0
 
+	""" utilities for the id
+	"""
+	def getId(self):
+		return id
+	
+	def setId(self, id):
+		self.id = id
+	
+	""" utilities for the position
+	"""
+	def toPosition(self, x, y):	
+		self.x = x
+		self.y = y
+	
+	def getPosition(self):
+		return (self.x, self.y)
+	
+	def setPosition(self, (x, y)):
+		self.x = x
+		self.y = y
+
+	""" handle images
+	"""
 	def readSprites(self):
-		
-		# more than one day for this purpose
+		# one full day to do this function
 		spritesheet = pygame.image.load(file(self.path))
 		spritesheet.convert()
 		sprites = [[0 for i in range(10)] for i in range(8)]
@@ -55,20 +79,42 @@ class Character:
 				sprites[x+4][y] = (spritesheet.subsurface((y * 64, (x + 12) * 64, 64, 64)))
 		
 		return sprites
-		
-	def toPosition(self, x, y):	
-		self.x = x
-		self.y = y
 	
+	def getImage(self, millis):
+		if self.arrow != [0, 0] or self.attackNow():
+			self.cycletime += millis
+			if self.cycletime > self.interval:
+				self.picnr[1] += 1
+				if self.picnr[1] == self.lenPic:
+					self.updateAttack()
+					self.updatePicnr()
+				self.cycletime = 0
+		return self.sprites[self.picnr[0]][self.picnr[1]]
+	
+	def updatePicnr(self):
+		if self.arrow == [0, -1]:  # up
+			self.picnr = [0, 0]
+			self.side = 'up'
+		elif self.arrow[0] == -1:  # left
+			self.picnr = [1, 0]
+			self.side = 'left'
+		elif self.arrow == [0, 1]: # down
+			self.picnr = [2, 0]
+			self.side = 'down'
+		elif self.arrow[0] == 1:   # right
+			self.picnr = [3, 0]
+			self.side = 'right'
+	
+	""" handle movement
+	"""
 	def move(self):
-		if self.attackNow():
-			return
+		#if self.attackNow(): return
 		if self.arrow[1] == -1:
 			self.moveUp()
-		if self.arrow[1] == 1:
-			self.moveDown()
 		if self.arrow[0] == -1:
 			self.moveLeft()
+		if self.arrow[1] == 1:
+			self.moveDown()
 		if self.arrow[0] == 1:
 			self.moveRight()
 	
@@ -89,79 +135,55 @@ class Character:
 			self.x += self.px
 		# print str(self.x) + ' + ' + str(self.y)
 	
-	def getImage(self, millis):
-		if self.arrow != [0, 0] or self.attackNow():
-			self.cycletime += millis
-			if self.cycletime > self.interval:
-				self.picnr[1] += 1
-				if self.picnr[1] == self.lenPic:
-					self.updateAttack()
-					self.updatePicnr()
-				self.cycletime = 0
-		return self.sprites[self.picnr[0]][self.picnr[1]]
-	
-	def getPosition(self):
-		return (self.x, self.y)
-	
-	def setPosition(self, (x, y)):
-		self.x = x
-		self.y = y
-	
+	""" handle arrow
+	"""
 	def setArrow(self, key, state):
 		self.arrow_states[key][0] = state
 		self.updateArrows()
 
 	def updateArrows(self):
-		# update arrow
-		self.arrow = [0, 0]
-		if self.arrow_states[pygame.K_UP][0]:
-			self.arrow[1] += self.arrow_states[pygame.K_UP][1]
-		if self.arrow_states[pygame.K_DOWN][0]:
-			self.arrow[1] += self.arrow_states[pygame.K_DOWN][1]
-		if self.arrow_states[pygame.K_LEFT][0]:
-			self.arrow[0] += self.arrow_states[pygame.K_LEFT][1]
-		if self.arrow_states[pygame.K_RIGHT][0]:
-			self.arrow[0] += self.arrow_states[pygame.K_RIGHT][1]
-		self.updatePicnr()
-	
-	def updatePicnr(self):
-		if self.arrow == [0, -1]:  # up
-			self.picnr = [0, 0]
-			self.side = 'up'
-		elif self.arrow[0] == -1:  # left
-			self.picnr = [1, 0]
-			self.side = 'left'
-		elif self.arrow == [0, 1]: # down
-			self.picnr = [2, 0]
-			self.side = 'down'
-		elif self.arrow[0] == 1:   # right
-			self.picnr = [3, 0]
-			self.side = 'right'
-	
+		if not self.attackNow():
+			self.arrow = [0, 0]
+			if self.arrow_states[pygame.K_UP][0]:
+				self.arrow[1] += self.arrow_states[pygame.K_UP][1]
+			if self.arrow_states[pygame.K_DOWN][0]:
+				self.arrow[1] += self.arrow_states[pygame.K_DOWN][1]
+			if self.arrow_states[pygame.K_LEFT][0]:
+				self.arrow[0] += self.arrow_states[pygame.K_LEFT][1]
+			if self.arrow_states[pygame.K_RIGHT][0]:
+				self.arrow[0] += self.arrow_states[pygame.K_RIGHT][1]
+			self.updatePicnr()
+
+	""" handle attack
+	"""
 	def attack(self, key):
 		if key == pygame.K_SPACE:
 			self.attack_keys[key] = True
-			self.slash()
+			self.attackKey = key
 			self.lenPic = 6
 			self.interval = 80
+			self.slash()
+			self.hit()
 	
 	def updateAttack(self):
-		for k in self.attack_keys.keys():
-			self.attack_keys[k] = False
-		if self.side == 'up':  # up
-			self.picnr = [0, 0]
-		elif self.side == 'left':  # left
-			self.picnr = [1, 0]
-		elif self.side == 'down': # down
-			self.picnr = [2, 0]
-		elif self.side == 'right':   # right
-			self.picnr = [3, 0]
-		self.lenPic = 9
-		self.interval = 150
+		if self.attackKey != 0:
+			if self.side == 'up':  # up
+				self.picnr = [0, 0]
+			elif self.side == 'left':  # left
+				self.picnr = [1, 0]
+			elif self.side == 'down': # down
+				self.picnr = [2, 0]
+			elif self.side == 'right':   # right
+				self.picnr = [3, 0]
+			self.attack_keys[self.attackKey] = False
+			self.lenPic = 9
+			self.interval = 150
+			self.attackKey = 0
 	
 	def attackNow(self):
 		if True in self.attack_keys.values():
 			return True
+		return False
 	
 	def slash(self):
 		if self.side == 'up':  # up
@@ -173,7 +195,25 @@ class Character:
 		elif self.side == 'right':   # right
 			self.picnr = [7, 0]
 	
-	# methods for bots
+	def hit(self):
+		if self.side == 'up':  # up
+			for x in range(self.x - 32, self.x + 32):
+				for y in range(self.y - 64, self.y):
+					print 'collide down'
+		elif self.side == 'left':  # left
+			for x in range(self.x - 64, self.x):
+				for y in range(self.y - 32, self.y + 32):
+					print 'collide left'
+		elif self.side == 'down': # down
+			for x in range(self.x - 32, self.x + 32):
+				for y in range(self.y, self.y + 64):
+					print 'collide down'
+		elif self.side == 'right':   # right
+			for x in range(self.x, self.x + 64):
+				for y in range(self.y - 32, self.y + 32):
+					print 'collide right'
+	
+	""" methods for bots """
 	def up(self):
 		self.arrow_states[pygame.K_UP][0] = True
 		self.arrow_states[pygame.K_LEFT][0] = False
