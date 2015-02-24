1fd9d635d56ade601f61cab6689a02e9ebe18d18
ordansantos <citycold.ordan@gmail.com>
2015-02-24 00:57:27 -0300

    Início do desenvolvimento do servidor

---
+++ src/ClientSocket.py
@@ -0,0 +1,36 @@
+
+
+import socket
+import json
+
+class ClientSocket :
+    
+    def __init__(self):
+        
+        self.host = 'localhost'
+        self.port = 8888
+        self.size = 1024
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect ((self.host, self.port))
+        data = self.conn.recv(self.size)
+        print data
+        
+    def get(self):
+       
+        self.sendMovement ("none")
+        data = self.conn.recv(self.size)
+        print '->', data
+        data = json.loads(data)
+        return data
+    
+    def sendMovement(self, movement):
+        
+        data = {"movement" : movement}
+        data = json.dumps(data)
+        self.conn.send (data)
+        
+        
+        
+        
+        
+        
--- src/Game.py
+++ src/Game.py
@@ -4,6 +4,7 @@ import Screen
 import Person
 import Sun
 import Bot
+import ClientSocket
 
 class Game:
     
@@ -20,7 +21,7 @@ class Game:
         self.sun = Sun.Sun()
         
         self.p = Person.Person.getNewPerson(100, 250, '../characters/ordan.png')
-        self.p.px= 4
+        self.p.px= 1
         Person.Person.setMaster(self.p.getId())
         
         Bot.Bot.putNewBot((1700, 1700), '../characters/skeleton.png')
@@ -33,6 +34,8 @@ class Game:
         #Bot.Bot.putNewBot((130, 501))
         #Bot.Bot.putNewBot((600, 800))
         #Bot.Bot.putNewBot((1000, 2000))
+        
+        self.client = ClientSocket.ClientSocket()
         
     def setScreenWidth(self, width):
         self.width = width
@@ -48,14 +51,38 @@ class Game:
             pygame.display.set_caption('%d %d - Sun Is Coming - Master(%d)' %(self.p.x, self.p.y, self.p.life))
             self.p.move()
             self.screen.draw(self.p, self.sun)
-            self.doEvent()
-    
+            #self.doEvent()
+            
+            client_event = self.client.get()
+            
+            for e in pygame.event.get():
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_LEFT:
+                        self.client.sendMovement("left")
+                    if e.key == pygame.K_RIGHT:
+                        self.client.sendMovement("right")
+                    if e.key == pygame.K_DOWN:
+                        self.client.sendMovement("down")
+                    if e.key == pygame.K_UP:
+                        self.client.sendMovement("up")
+                if e.type == pygame.KEYUP:
+                    self.p.stopped()
+            for e in client_event['moves']:
+                if e == 'up':
+                    self.p.up()
+                if e == 'down':
+                    self.p.down()
+                if e == 'left':
+                    self.p.left()
+                if e == 'right':
+                    self.p.right()
+                
     def doEvent(self):
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 self.running = False
                 pygame.quit()
-            
+        """  
             if e.type == pygame.KEYUP:
                 if e.key in self.p.arrow_states.keys():
                     self.p.setArrow(e.key, False)
@@ -66,3 +93,5 @@ class Game:
                     self.p.attack(e.key)
                 if e.key == pygame.K_f:
                     self.p.updateFurtiveness()
+                    """
+                    
